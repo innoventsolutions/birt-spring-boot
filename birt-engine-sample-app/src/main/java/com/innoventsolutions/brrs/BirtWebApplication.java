f7270ec38384c4cca8ef54b0538016c40d480649
Steve Schafer <sschafer@innoventsolutions.com>
2020-02-12 15:57:50 -0700

    Make sample app a web application supporting /run.  Does not use eureka or zuul.

---
+++ birt-engine-sample-app/src/main/java/com/innoventsolutions/brrs/BirtWebApplication.java
@@ -0,0 +1,11 @@
+package com.innoventsolutions.brrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirtWebApplication {
+	public static void main(final String[] args) {
+		SpringApplication.run(BirtWebApplication.class, args);
+	}
+}
