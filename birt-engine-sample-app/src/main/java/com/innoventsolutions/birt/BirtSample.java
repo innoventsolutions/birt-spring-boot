ebe84e8dd0e35c705f90658d706626d46da704f6
Scott Rosenbaum <scottr@innoventsolutions.com>
2020-03-12 15:18:19 -0500

    Small cleanup, trying to figure out sample-app

--- birt-engine-sample-app/src/main/java/com/innoventsolutions/birt/BirtSample.java
+++ birt-engine-sample-app/src/main/java/com/innoventsolutions/birt/BirtSample.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BirtWebApplication {
+public class BirtSample {
 	public static void main(final String[] args) {
-		SpringApplication.run(BirtWebApplication.class, args);
+		SpringApplication.run(BirtSample.class, args);
 	}
 }
