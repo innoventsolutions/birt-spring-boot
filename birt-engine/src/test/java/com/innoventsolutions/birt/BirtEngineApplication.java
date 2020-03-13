230fa467ff0180fc5d92b0f66aea0b7ee87f4da8
Steve Schafer <sschafer@innoventsolutions.com>
2020-03-13 12:30:54 -0600

    Move BirtEngineApplication from main to test. Comment out spring-boot-maven-plugin

--- birt-engine/src/test/java/com/innoventsolutions/birt/BirtEngineApplication.java
+++ birt-engine/src/test/java/com/innoventsolutions/birt/BirtEngineApplication.java
@@ -9,8 +9,12 @@
  ******************************************************************************/
 package com.innoventsolutions.birt;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BirtEngineApplication {
@@ -19,4 +23,10 @@ public class BirtEngineApplication {
 		SpringApplication.run(BirtEngineApplication.class, args);
 	}
 
+	@Bean(name = "submitJobExecutor")
+	public ExecutorService taskExecutor() {
+		final ExecutorService executor = Executors.newFixedThreadPool(10);
+
+		return executor;
+	}
 }
