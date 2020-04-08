8f32dbde6df4fdac0b5f1822374c54e43d3db860
Steve Schafer <sschafer@innoventsolutions.com>
2020-04-08 14:38:23 -0600

    change snapshot versions

--- birt-engine-sample-app-full/src/main/java/sample/birt/security/SecurityTokenConfig.java
+++ birt-engine-sample-app-full/src/main/java/sample/birt/security/SecurityTokenConfig.java
@@ -30,7 +30,7 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
 				.authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
 				// Add a filter to validate the tokens with every request
 				.addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
-				.addFilter(new JwtTokenAuthenticationFilter(jwtConfig))
+				// .addFilter(new JwtTokenAuthenticationFilter(jwtConfig))
 				// authorization requests config
 				.authorizeRequests()
 				// allow all who are accessing "auth" service
